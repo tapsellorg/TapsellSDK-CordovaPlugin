7bf1021698cba56b9bcd18c60d5ae13b7e5b82f2
Alireza Khojastehfar <a.r.khojastehfar@gmail.com>
2018-07-17 17:41:21 +0430

    fix circle ci config

--- www/tapsell.js
+++ www/tapsell.js
@@ -15,7 +15,8 @@ window.tapsell_banner_320x50 = 1;
 window.tapsell_banner_320x100 = 2;
 window.tapsell_banner_250x250 = 3;
 window.tapsell_banner_300x250 = 4;
-var tapsellPluginVersion = 3.1.13;//???????
+
+var tapsellPluginVersion = "3.1.13";
 
 module.exports = {
 	initialize: function(appKey) {
