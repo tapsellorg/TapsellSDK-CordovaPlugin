e9d4c6ad79b64ac5655ab59a1fa3db18237f2ab0
Alireza Khojastehfar <a.r.khojastehfar@gmail.com>
2018-07-17 09:17:36 +0430

    circleCI and newVersion script added

---
+++ newVersion.sh
@@ -0,0 +1,21 @@
+if [[ "$1" eq "" ]]; then
+    echo "please enter the new version"; exit 1
+fi
+if [[ "$2" eq "" ]]; then
+    echo "please enter tapsell sdk version"; exit 1
+fi
+pluginVersion=$1
+sdkVersion=$2
+wget https://storage.backtory.com/tapsell-server/sdk/cordova/tapsell-sdk-cordova-$sdkVersion.jar
+rm ./src/android/*.jar
+mv tapsell-sdk-cordova-$sdkVersion.jar ./src/android/tapsell-cordova-v$sdkVersion.jar
+newLine="       <source-file src=\"src/android/tapsell-cordova-v$sdkVersion.jar\" target-dir=\"libs\" />"
+sed -i "/source-file src=\"src/android/tapsell-cordova-/c\ $newLine" plugin.xml
+newLine="var tapsellPluginVersion = $pluginVersion;"
+sed -i "/"tapsellPluginVersion"/c\ $newLine" www/tapsell.js
+sed -i "5s/.*/version=\"$\">" plugin.xml
+rm package.json
+plugman createpackagejson
+git add --all
+git commit -m "updating for plugin version $pluginVersion"
+git push
