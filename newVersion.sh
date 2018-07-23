2c5723ee7b94d3b3da0e0902b712e560a0c29a99
Alireza Khojastehfar <a.r.khojastehfar@gmail.com>
2018-07-23 16:00:52 +0430

    new version acript bug fix

--- newVersion.sh
+++ newVersion.sh
@@ -8,15 +8,22 @@ fi
 git checkout master
 pluginVersion=$1
 sdkVersion=$2
+echo $pluginVersion
+echo $sdkVersion
 wget https://storage.backtory.com/tapsell-server/sdk/cordova/tapsell-sdk-cordova-$sdkVersion.jar
 rm ./src/android/*.jar
 mv tapsell-sdk-cordova-$sdkVersion.jar ./src/android/tapsell-cordova-v$sdkVersion.jar
+echo "replaced jar"
 newLine="       <source-file src=\"src/android/tapsell-cordova-v$sdkVersion.jar\" target-dir=\"libs\" />"
 sed -i "/source-file src=\"src/android/tapsell-cordova-/c\ $newLine" plugin.xml
+echo "updated plugin.xml"
 newLine="var tapsellPluginVersion = $pluginVersion;"
-sed -i "/"tapsellPluginVersion ="/c\ $newLine" www/tapsell.js
+sed -i "/\"tapsellPluginVersion =\"/c\ $newLine" www/tapsell.js
+echo "updated www/tapsell.js"
 sed -i "5s/.*/version=\"$tapsellPluginVersion\">/" plugin.xml
-sed -i "2s/.*/version\": "$tapsellPluginVersion\",/" package.json
+echo "updated plugin.xml"
+sed -i "2s/.*/version\": \"$tapsellPluginVersion\",/" package.json
+echo "updated package.json"
 git add --all
 git commit -m "updating for plugin version $pluginVersion"
-git push
+#git push
